647f668a31863f05826b8993e5bfb56ad088e2dc
성백견 <beksung@gmail.com>
2019-07-02 17:12:43 +0900

    권순성 변경사항 적용

--- app/src/main/java/kr/co/theunify/wear/activity/InstructionActivity.java
+++ app/src/main/java/kr/co/theunify/wear/activity/InstructionActivity.java
@@ -1,27 +1,46 @@
 package kr.co.theunify.wear.activity;
 
 import android.os.Bundle;
+import android.text.util.Linkify;
 import android.view.View;
 import android.widget.TextView;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import butterknife.BindView;
+import butterknife.ButterKnife;
 import butterknife.OnClick;
 import kr.co.theunify.wear.R;
 
 public class InstructionActivity extends BaseActivity {
 
 
-    @BindView(R.id.btn_instruction)
-    TextView btn_confirm;
+
+    @BindView(R.id.tx_smartpouch)
+    TextView tx_smartpouch;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.a_instruction);
+        ButterKnife.bind(this);
+        InitView();
+    }
 
+    public void InitView(){
+        Linkify.TransformFilter mTransform = new Linkify.TransformFilter() {
+            @Override
+            public String transformUrl(Matcher match, String url) {
+                return "";
+            }
+        };
+        Pattern pattern = Pattern.compile(getString(R.string.SmartPouch_Url));
 
+        Linkify.addLinks(tx_smartpouch,pattern,"http://thesmartpouch.com/",null,mTransform);
     }
 
     @OnClick(R.id.btn_confirm)
